32ca21d7d65efda0466a01f0b9eb2d9f7afd83c0
RAJATHYB <rajathyb23@gmail.com>
2023-10-19 12:34:37 +0530

    SECOND

---
+++ Tri.java
@@ -0,0 +1,13 @@
+class Tri
+{
+static void tri(int b,int h)
+{
+double area=0.5*(b*h);
+System.out.println(area);
+}
+public static void main(String[] args)
+{
+System.out.println("Area of Triangle");
+tri(6,3);
+}
+}
